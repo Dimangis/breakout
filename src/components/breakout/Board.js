d7d0af21bb31522b1ba9553101b91b3ede75dca8
Diangis <nissan200oe@yandex.ru>
2024-07-02 10:42:44 +0300

    Added player platform, styles has benn changed

--- src/components/breakout/Board.js
+++ src/components/breakout/Board.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useRef } from "react";
 import { BallMovement } from "./BallMovement";
 import data from "../../data";
-
+import WallCollision from "../utils/WallCollision";
+import Paddle from "./Paddle";
+let { ballObj, paddleProps } = data;
 export default function Board() {
   const canvasRef = useRef(null);
 
@@ -10,31 +12,27 @@ export default function Board() {
       const canvas = canvasRef.current;
       const ctx = canvas.getContext("2d");
 
-      let { ballObj } = data;
-
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
       BallMovement(ctx, ballObj);
 
-      if (
-        ballObj.y - ballObj.rad <= 0 ||
-        ballObj.y + ballObj.rad >= canvas.height
-      ) {
-        ballObj.dy *= -1;
-      }
+      WallCollision(ballObj, canvas);
 
-      if (
-        ballObj.x + ballObj.rad >= canvas.width ||
-        ballObj.x - ballObj.rad <= 0
-      ) {
-        ballObj.dx *= -1;
-      }
+      Paddle(ctx, canvas, paddleProps);
 
       requestAnimationFrame(render);
     };
     render(); //обертка для многократного вызова рендера
   }, []);
   return (
-    <canvas id="canvas" ref={canvasRef} height="500px" width="800px"></canvas>
+    <canvas
+      id="canvas"
+      ref={canvasRef}
+      onMouseMove={
+        (event) => (paddleProps.x = event.clientX - paddleProps.width / 2 - 10) //центрируем платформу относительно курсора
+      }
+      height="500px"
+      width="800px"
+    ></canvas>
   );
 }
